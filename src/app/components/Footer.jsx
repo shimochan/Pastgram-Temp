70fdb44a1626b0c35057a6d802ef43b1ded9a58e
Tatsuya Tanaka <kanatatsu64@gmail.com>
2023-08-26 00:59:37 +0900

    display current location

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { HomeOutlined, AddAPhotoOutlined, AccountCircleOutlined } from '@mui/icons-material';
-import styles from "./Footer.module.css"
+import { Home, HomeOutlined,
+         AddAPhoto, AddAPhotoOutlined,
+         AccountCircle, AccountCircleOutlined
+       } from '@mui/icons-material';
+import { getPath } from '../lib/url';
+import styles from "./Footer.module.css";
+import mobile from "../mobile.module.css";
+
+const isHome = (path) => path === "/home";
+const isAdd = (path) => path === "/add";
+const isUser = (path) => path === "/user";
 
 export function Footer() {
-  return (
-    <footer className={styles.container}>
-      <BottomNavigation showLabels className={styles.icon}>
-        <BottomNavigationAction style={{color:"black"}} icon={<HomeOutlined />} />
-        <BottomNavigationAction style={{color:"black"}} icon={<AddAPhotoOutlined />} />
-        <BottomNavigationAction style={{color:"black"}} icon={<AccountCircleOutlined />} />
-      </BottomNavigation>
+  const path = getPath();
 
+  const HomeIcon = isHome(path) ? Home : HomeOutlined;
+  const AddIcon = isAdd(path) ? AddAPhoto : AddAPhotoOutlined;
+  const UserIcon = isUser(path) ? AccountCircle : AccountCircleOutlined;
+
+  return (
+    <footer className={[styles.container, mobile.window_width].join(' ')}>
+      <BottomNavigation showLabels className={styles.icon}>
+        <BottomNavigationAction style={{color:"black"}} icon={<HomeIcon />} />
+        <BottomNavigationAction style={{color:"black"}} icon={<AddIcon />} />
+        <BottomNavigationAction style={{color:"black"}} icon={<UserIcon />} />
+      </BottomNavigation>
     </footer>
   );
 }
---
+++ src/app/lib/url.js
@@ -0,0 +1,19 @@
+import { headers } from "next/headers";
+
+export function getDomain() {
+    const headersList = headers();
+    const domain = headersList.get("x-forwarded-host") || "";
+    return domain;
+}
+
+export function getProtocol() {
+    const headersList = headers();
+    const protocol = headersList.get("x-forwarded-proto") || "";
+    return protocol;
+}
+
+export function getPath() {
+    const headersList = headers();
+    const pathname = headersList.get("x-invoke-path") || "";
+    return pathname;
+}
