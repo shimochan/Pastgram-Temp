924a29b997673145ba156a4bed4f259e796ce4c4
shimochan <keita.s0412@icloud.com>
2023-08-27 00:08:50 +0900

    投稿詳細写真遷移

--- src/app/postDetails/page.jsx
+++ src/app/postDetails/page.jsx
@@ -1,12 +1,18 @@
-
+'use client'
 
 import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
+// import { Footer } from '../components/Footer';
 import styles from "./postDetails.module.css";
 import Image from 'next/image';
 import { Avatar, Typography } from '@mui/material';
 import React from 'react';
 import LikeButton from './LikeButton';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+const images = ['/sample.jpg', '/sample2.jpg', '/sample3.jpg']
 
 export default function Home() {
     return (
@@ -17,13 +23,34 @@ export default function Home() {
                     <Avatar className={styles.avatar}>H</Avatar>
                     <Typography className={styles.username}>Username</Typography>
                 </div>
-                <Image src="/sample.jpg" alt="photo" width="0" height="0" sizes="100vw" className={styles.image} />
+                <div>
+                    <Swiper
+                        pagination={{
+                            dynamicBullets: true,
+                        }}
+                        modules={[Pagination]}
+                        className="mySwiper"
+                    >
+                        {images.map(src => {
+                            return (
+                                <SwiperSlide >
+                                    <Image src={src} layout="responsive" width={640} height={400} alt="test_image"  />
+                                </SwiperSlide>
+                            )
+                        })}
+                    </Swiper>
+                </div>
+              
                 <div className={styles.goodbutton}>
                     懐かしイイね
-                        <LikeButton />
+                    <LikeButton />
                 </div>
+
+
+
             </main>
-            <Footer />
+
+            {/* <Footer /> */}
         </>
     )
 }
