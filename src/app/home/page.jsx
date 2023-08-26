292a3077cd65a72a50d179c6aff33953c42262cc
Tatsuya Tanaka <kanatatsu64@gmail.com>
2023-08-26 10:23:16 +0900

    hot fix (home.module.css, crossOrigin)

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
-import styles from "./home.module.css";
+import styles from "./Home.module.css";
 import mobile from "../mobile.module.css";
 
 export default function Home() {
