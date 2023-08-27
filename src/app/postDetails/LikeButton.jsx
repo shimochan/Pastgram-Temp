b9c0243166b6c8701421ed8c962b57db17603156
yuka <gem60240@kwansei.ac.jp>
2023-08-28 07:59:50 +0900

    add good button change

--- src/app/postDetails/LikeButton.jsx
+++ src/app/postDetails/LikeButton.jsx
@@ -1,25 +1,54 @@
 'use client'
 
 import React, { useState } from 'react';
-import Button from '@mui/material/Button';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 
 function LikeButton() {
-  const [liked, setLiked] = useState(false);
+  // const [liked, setLiked] = useState(false);
+  const [hovered, setHovered] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   const handleLikeClick = () => {
-    setLiked(!liked);
+    setHovered(true);
+    setClicked(true);
+    setTimeout(() => {
+      setClicked(false);
+    }, 100);
+  };
+
+  const handleMouseEnter = () => {
+    setHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHovered(false);
+    setClicked(false);
+  };
+
+  const size = clicked ? '20px' : '24px';
+  const style = {
+    width: size,
+    height: size
   };
 
   return (
-    <Button
-      variant={liked ? 'outlined' : 'contained'}
-      color="primary"
-      startIcon={<ThumbUpIcon />}
+    <div
+      style={{
+        display: 'inline-block',
+        cursor: 'pointer',
+        paddingLeft: '5px',
+        //fontSize: clicked ? '18px' : '30px',
+        //padding: clicked ? '5px' : '0px',
+        transition: 'font-size 0.1s, color 0.3s',
+        color: '#6da7e7',
+      }}
       onClick={handleLikeClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
-      
-    </Button>
+      {hovered ? <ThumbUpIcon style={style} /> : <ThumbUpOffAltIcon style={style} />}
+    </div>
   );
 }
 
