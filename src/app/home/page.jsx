1ad4a5bbef1a1e983cd07167e867cc606299f405
shimochan <keita.s0412@icloud.com>
2023-08-27 15:31:24 +0900

    home画面写真追加

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -3,27 +3,48 @@ import { Footer } from '../components/Footer';
 import styles from "./home.module.css";
 import Image from 'next/image';
 
+const ITEMS = [
+
+  {
+    src: "/sample.jpg",
+    date:"2015/11/11",
+  },
+  {
+    src: "/sample2.jpg",
+    date:"2015/11/11",
+  },
+  {
+    src: "/sample3.jpg",
+    date:"2015/11/11",
+  },
+  {
+    src: "/sample4.jpg",
+    date:"2015/11/11",
+  },
+
+];
+ 
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.flame}>
         <div className={styles.grid}>
-          <div className={styles.square1} >
-            <Image src="/pin.png" alt="pin" width="40" height="40" className={styles.pin} />
-          </div>
 
-          <div className={styles.square2} >
-            <Image src="/pin.png" alt="pin" width="40" height="40" className={styles.pin} />
-          </div>
+          {ITEMS.map((item) => {
+            const index = Math.floor(Math.random()*8) + 1;
+            return(
+            <div className={styles[`square${index}`]} >
+              <Image src="/pin.png" alt="pin" width="40" height="40" className={styles.pin} />
+              <Image src={item.src} alt="photo" width="0" height="0" sizes="100vw" className={styles.photo} />
+              <div className={styles.date}>
+                {item.date}
+              </div>
+            </div>
+            );
 
-          <div className={styles.square3} >
-            <Image src="/pin.png" alt="pin" width="40" height="40" className={styles.pin} />
-          </div>
+          })}
 
-          <div className={styles.square4} >
-            <Image src="/pin.png" alt="pin" width="40" height="40" className={styles.pin} />
-          </div>
         </div>
       </main>
 
